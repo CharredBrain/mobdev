04f6cf612cc1235b1eb2626f263e173f9803e701
CharredBrain <mev.sverchok@yandex.ru>
2024-03-07 02:05:10 +0300

    i am tired
    outta my mind

---
+++ lesson1/buttonclicker/src/main/java/ru/mirea/maksimovaok/buttonclicker/MainActivity.java
@@ -0,0 +1,45 @@
+package ru.mirea.maksimovaok.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+    private CheckBox checkBox;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewStudent = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № 13");
+                checkBox.toggle();
+            }
+        };
+
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+    public void onButtonClick(View view) {
+        checkBox.toggle();
+        textViewStudent.setText("Починяю примус");
+    }
+
+}
