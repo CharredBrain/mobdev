cc699222cdced72bf8e6c599306ba164a5214474
CharredBrain <mev.sverchok@yandex.ru>
2024-05-22 14:51:15 +0300

    eployeedb fixed
    module fixed

--- Lesson6/employeedb/src/main/java/ru/mirea/maksimovaok/employeedb/MainActivity.java
+++ Lesson6/employeedb/src/main/java/ru/mirea/maksimovaok/employeedb/MainActivity.java
@@ -1,24 +1,38 @@
 package ru.mirea.maksimovaok.employeedb;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        AppDatabase db = App.getInstance().getDatabase();
+        EmployeeDao employeeDao = db.employeeDao();
+        Employee employee = new Employee();
+        employee.id = 1;
+        employee.name = "One-Punch-Man";
+        employee.salary = 10000;
+        Employee friend = new Employee();
+        friend.id = 100;
+        friend.name = "That friend who always helps everyone";
+        friend.salary = 0;
+
+        employeeDao.insert(employee);
+        employeeDao.insert(friend);
+
+        List<Employee> employees = employeeDao.getAll();
+
+        employee = employeeDao.getById(100);
+
+        friend.salary = 666;
+        employeeDao.update(friend);
+        Log.d("DB", employee.name + " " + employee.salary);
+        Log.d("DB", friend.name + " " + friend.salary);
     }
 }
