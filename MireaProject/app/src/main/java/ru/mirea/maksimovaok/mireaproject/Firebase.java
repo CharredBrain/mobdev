0e78ffa3524597ea13820e4ba1433f3308f0e2a6
CharredBrain <mev.sverchok@yandex.ru>
2024-06-07 18:30:02 +0300

    firebase fix
    fix for firebase fragment

--- MireaProject/app/src/main/java/ru/mirea/maksimovaok/mireaproject/Firebase.java
+++ MireaProject/app/src/main/java/ru/mirea/maksimovaok/mireaproject/Firebase.java
@@ -2,46 +2,40 @@ package ru.mirea.maksimovaok.mireaproject;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Firebase#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+
+import java.util.Objects;
+
+import ru.mirea.maksimovaok.mireaproject.databinding.FragmentFirebaseBinding;
+
 public class Firebase extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FragmentFirebaseBinding binding;
+    private FirebaseAuth mAuth;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     public Firebase() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Firebase.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Firebase newInstance(String param1, String param2) {
         Firebase fragment = new Firebase();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +43,126 @@ public class Firebase extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_firebase, container, false);
+        mAuth = FirebaseAuth.getInstance();
+
+        binding = FragmentFirebaseBinding.inflate(inflater, container, false);
+        binding.signInButton.setOnClickListener(v ->
+                signIn(
+                        binding.emailEdit.getText().toString(),
+                        binding.passwordEdit.getText().toString()
+                )
+        );
+        binding.registerButton.setOnClickListener(v ->
+                createAccount(
+                        binding.emailEdit.getText().toString(),
+                        binding.passwordEdit.getText().toString()
+                )
+        );
+        binding.signOutButton.setOnClickListener(v -> signOut());
+        binding.verifyEmailButton.setOnClickListener(v -> sendEmailVerification());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            binding.statusTextView.setText(getString(R.string.emailpassword_status_fmt,
+                    user.getEmail(), user.isEmailVerified()));
+            binding.detailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+            binding.emailPasswordButtons.setVisibility(View.GONE);
+            binding.emailPasswordFields.setVisibility(View.GONE);
+            binding.signedInButtons.setVisibility(View.VISIBLE);
+            binding.verifyEmailButton.setEnabled(!user.isEmailVerified());
+        } else {
+            binding.statusTextView.setText(R.string.signed_out);
+            binding.detailTextView.setText(null);
+            binding.emailPasswordButtons.setVisibility(View.VISIBLE);
+            binding.emailPasswordFields.setVisibility(View.VISIBLE);
+            binding.signedInButtons.setVisibility(View.GONE);
+        }
+    }
+
+    public void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure",
+                                    task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    public void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                        if (!task.isSuccessful()) {
+                            binding.statusTextView.setText(R.string.auth_failed);
+                        }
+                    }
+                });
+    }
+
+    public void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+    }
+
+    public void sendEmailVerification() {
+        binding.verifyEmailButton.setEnabled(false);
+        final FirebaseUser user = mAuth.getCurrentUser();
+        Objects.requireNonNull(user).sendEmailVerification()
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        binding.verifyEmailButton.setEnabled(true);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getActivity(),
+                                    "Verification email sent to " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(getActivity(),
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
